419287548e3e353731417d7886e8788c6f26bd2d
Gustavo Soares <gustavo41674@fiec.edu.br>
2025-08-21 21:53:23 -0300

    commit inicial

---
+++ src/main/java/com/fiec/projeto_back_gastroflow/ProjetoBackGastroflowApplication.java
@@ -0,0 +1,13 @@
+package com.fiec.projeto_back_gastroflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBackGastroflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBackGastroflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiec/projeto_back_gastroflow/ProjetoBackGastroflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiec.projeto_back_gastroflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBackGastroflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
