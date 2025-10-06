3728b64f7d47ec62b06c575aa91f93644f6c74bc
Gustavo Soares <gustavo.soares220@hotmail.com>
2025-10-06 19:12:08 -0300

    Resolvendo pre-authoriation

--- src/main/java/com/fiec/projeto_back_gastroflow/features/admin/controllers/AdminController.java
+++ src/main/java/com/fiec/projeto_back_gastroflow/features/admin/controllers/AdminController.java
@@ -2,6 +2,7 @@ package com.fiec.projeto_back_gastroflow.features.admin.controllers;
 
 import com.fiec.projeto_back_gastroflow.features.user.models.User;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/api/admin")
 public class AdminController {
 
-    @Secured("ADMIN")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping
     public void testRequest(Authentication authentication){
         User user = (User) authentication.getPrincipal();
