68339c1867b7780b40b689544f9ae72f1121e7f9
Gustavo Soares <gustavo.soares220@hotmail.com>
2025-10-16 19:38:08 -0300

    Em progresso: Integração com firebase

--- src/main/java/com/fiec/projeto_back_gastroflow/features/aula/controllers/AulaController.java
+++ src/main/java/com/fiec/projeto_back_gastroflow/features/aula/controllers/AulaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/aulas")
+@RequestMapping("/aula")
 @AllArgsConstructor
 public class AulaController {
 
