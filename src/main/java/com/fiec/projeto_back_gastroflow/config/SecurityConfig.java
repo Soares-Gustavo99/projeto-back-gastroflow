52805cee37c43b6855938ba6aee852de6034b542
Gustavo Soares <gustavo.soares220@hotmail.com>
2025-10-22 20:35:43 -0300

    Arrumado umas coisa

--- src/main/java/com/fiec/projeto_back_gastroflow/config/SecurityConfig.java
+++ src/main/java/com/fiec/projeto_back_gastroflow/config/SecurityConfig.java
@@ -42,17 +42,6 @@ public class SecurityConfig {
                 })
                 .authenticationProvider(authenticationProvider)
                 .authorizeHttpRequests(requests -> requests
-                                .requestMatchers("/images/**",
-                                        "/v1/api/auth/**",
-                                        "/v1/api/users/admin",
-                                        "/v1/api/users/guest",
-                                        "/v1/api/users/standard",
-                                        "/v1/api/notifications/sendToUser",
-                                        "/swagger-ui/**",
-                                        "/swagger-ui.html",
-                                        "/actuator/health"
-                                ).permitAll()
-
                         .requestMatchers("/images/**",
                                 "/v1/api/auth/**",
                                 "v1/api/users/admin/**",
