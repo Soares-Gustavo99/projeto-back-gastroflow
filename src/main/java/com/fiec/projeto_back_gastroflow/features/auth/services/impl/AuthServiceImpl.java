c6b4e046a04e4297335b4901f98eed3afe70545a
Wendelllllllll <wendel38315@fiec.edu.br>
2025-11-17 17:53:15 -0300

    Mudanças no arquivo AuthServiceImpl

--- src/main/java/com/fiec/projeto_back_gastroflow/features/auth/services/impl/AuthServiceImpl.java
+++ src/main/java/com/fiec/projeto_back_gastroflow/features/auth/services/impl/AuthServiceImpl.java
@@ -3,10 +3,11 @@ package com.fiec.projeto_back_gastroflow.features.auth.services.impl;
 import com.fiec.projeto_back_gastroflow.features.auth.dto.LoginRequest;
 import com.fiec.projeto_back_gastroflow.features.auth.dto.RegisterRequest;
 import com.fiec.projeto_back_gastroflow.features.auth.services.AuthService;
+import com.fiec.projeto_back_gastroflow.features.user.models.Standard;
 import com.fiec.projeto_back_gastroflow.features.user.models.User;
 import com.fiec.projeto_back_gastroflow.features.user.models.UserLevel;
+import com.fiec.projeto_back_gastroflow.features.user.repositories.StandardRepository;
 import com.fiec.projeto_back_gastroflow.features.user.repositories.UserRepository;
-import com.fiec.projeto_back_gastroflow.features.user.services.UserService;
 import com.fiec.projeto_back_gastroflow.utils.PasswordEncryptor;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -18,19 +19,33 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
+    private final StandardRepository standardRepository;
     private final PasswordEncoder passwordEncoder;
-
 
     @Override
     public User register(RegisterRequest request) {
+
+        // Verifica email duplicado
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RuntimeException("Email já está em uso");
+        }
+
+        // Cria o User base
         User user = new User();
         user.setName(request.getName());
         user.setEmail(request.getEmail());
         user.setPassword(passwordEncoder.encode(request.getPassword()));
-        user.setAccessLevel(UserLevel.ROLE_STANDARD); // Define o nível de acesso padrão
+        user.setAccessLevel(UserLevel.ROLE_STANDARD);
         user.setPicture(request.getPicture());
 
-        return userRepository.save(user);
+        User savedUser = userRepository.save(user);
+
+        // Cria o registro na tabela STANDARD
+        Standard standard = new Standard();
+        standard.setUser(savedUser);
+        standardRepository.save(standard);
+
+        return savedUser;
     }
 
     @Override
