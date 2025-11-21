98195b408b5005c715d57acae8dca60fd2d23757
Gustavo Soares <gustavo.soares220@hotmail.com>
2025-11-21 12:15:33 -0300

    ajustes no system user

--- src/main/java/com/fiec/projeto_back_gastroflow/features/user/models/SystemUser.java
+++ src/main/java/com/fiec/projeto_back_gastroflow/features/user/models/SystemUser.java
@@ -22,6 +22,6 @@ public abstract class SystemUser {
     @Column(updatable = false, nullable = false)
     private UUID id;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     User user;
 }
